ec00ac0c46f85ff2cdf785d98a1522256d38eacd
nikolaytuz <nikolaytuz56@gmail.com>
2020-12-03 22:22:04 +0400

    Добавил dist

---
+++ dist/js/chunk-2d2378b2.d63632f6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d2378b2"],{fc21:function(e,n,t){"use strict";t.r(n);var s=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("This is an Planets page")])])}],c=t("2877"),l={},u=Object(c["a"])(l,s,a,!1,null,null,null);n["default"]=u.exports}}]);
+//# sourceMappingURL=chunk-2d2378b2.d63632f6.js.map
